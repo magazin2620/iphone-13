3f0b493bb979c63456bf1abd3e31a01821f8100a
magazin2620 <magazin2620@gmail.com>
2022-08-15 06:49:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,121 @@
+// Page 1 Pin
+const tlIntro = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.first-page',
+    start: '0%',
+    end: '100%',
+    pin: true,
+    pinSpacing: false,
+  },
+});
+
+// Page 2 Highlight
+const tlH = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.second-page',
+    scrub: true,
+    start: '-40%',
+    end: '40%',
+  },
+});
+
+tlH.fromTo(
+  '.highlight',
+  { color: 'rgba(255,255,255, 0.4' },
+  { color: 'rgba(255,255,255, 1', stagger: 1 }
+);
+
+const tlHRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.second-page',
+    scrub: true,
+    start: '-20%',
+    end: '60%',
+  },
+});
+
+tlHRemove.to('.highlight', { color: 'rgba(255,255,255, 0.4', stagger: 1 });
+
+// Page 3
+const tlSplit = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.third-page',
+    start: '-25%',
+    end: '30%',
+    scrub: true,
+  },
+});
+
+tlSplit
+  .fromTo('.large-phone', { x: '40%' }, { x: '20%' })
+  .fromTo('.small-phone', { x: '-40%' }, { x: '-20%' }, '<')
+  .fromTo(
+    '.product-text-left',
+    { x: 100, opacity: 0 },
+    { opacity: 1, x: 0 },
+    '<'
+  )
+  .fromTo(
+    '.product-text-right',
+    { x: -100, opacity: 0 },
+    { opacity: 1, x: 0 },
+    '<'
+  );
+
+const tlSplitPin = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.third-page',
+    pin: true,
+    pinSpacing: false,
+    start: '0%',
+    end: '100%',
+  },
+});
+
+// Page 4 Carousel
+const swatches = document.querySelectorAll('.swatches img');
+const gallery = document.querySelector('.phone-gallery');
+const slides = document.querySelectorAll('.phone-gallery-container');
+
+let currentSwatch = 'blue';
+let topIndex = 2;
+
+swatches.forEach((swatch, index) => {
+  const coord = slides[index].getBoundingClientRect().left;
+
+  swatch.addEventListener('click', (e) => {
+    let swatchName = e.target.getAttribute('swatch');
+    let closeUp = document.querySelector('.' + swatchName);
+    // Check if we are on the same swatch
+    if (currentSwatch === swatchName) return;
+
+    gsap.set(closeUp, { zIndex: topIndex });
+    gsap.fromTo(closeUp, { opacity: 0 }, { opacity: 1, duration: 1 });
+
+    // Gallery
+    gsap.to(gallery, { x: -coord, duration: 1, ease: 'back.out(1)' });
+    // Increment zIndex
+    topIndex++;
+    currentSwatch = swatchName;
+  });
+});
+
+// Page 5 Video on scroll
+const tlVideo = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.fifth-page',
+    start: '0%',
+    end: '150%',
+    scrub: true,
+    pin: true,
+  },
+});
+
+tlVideo
+  .fromTo('.product-video', { currentTime: 0 }, { currentTime: 3, duration: 1 })
+  .fromTo(
+    '.product-info-container h3',
+    { opacity: 0 },
+    { opacity: 1, stagger: 0.25, duration: 0.5 },
+    '<'
+  );
